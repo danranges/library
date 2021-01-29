751cece2baad0ebceb3aba32c90070b19e7d02f9
Dan Ranges <danranges@gmail.com>
2021-01-29 19:48:31 +0100

    Add form draft, add-new-book feature

--- library.js
+++ library.js
@@ -1,13 +1,31 @@
+const library = []
+const btnAddBook = document.getElementById('btn-add-book')
+const form = document.getElementById('add-book-form')
+
+
+
 function Book(title, author, pages, read) {
     this.title = title
     this.author = author
     this.pages = pages
     this.read = read
-    this.info = () => {
-        return `${title} by ${author}, ${pages} pages, ${read ? 'read' : 'not read yet'}`
-    }
 }
 
-const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false)
+function addBookToLibrary() {
+    library.push(new Book(newTitle, newAuthor, newPages, newRead))
+}
 
-console.log(theHobbit.info())
+
+
+library.push(new Book('The Hobbit', 'J.R.R. Tolkien', 295, false))
+library.push(new Book('The DaVinci Code', 'Dan Brown', 384, true))
+library.push(new Book('Mitt Liv', 'Lars Monsen', 246, false))
+
+btnAddBook.addEventListener('click', () => {
+    library.push(new Book(form.elements[0].value,               //title
+                          form.elements[1].value,               //author
+                          parseInt(form.elements[2].value),     //pages
+                          form.elements[3].checked))            //read 
+    console.table(library)
+    }
+)
