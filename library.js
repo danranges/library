e68efd9f07148fd68c95f0e18cd7eda6ef8b8b69
Dan Ranges <danranges@gmail.com>
2021-01-31 16:17:19 +0100

    Add stored card creation function, card styling

--- library.js
+++ library.js
@@ -3,8 +3,7 @@ const btnAddBook = document.getElementById('btn-add-book')
 const form = document.getElementById('add-book-form')
 const formOverlay = document.getElementById('add-book-overlay')
 const btnBookForm = document.getElementById('btn-open-add-new')
-
-
+const cardCatalog = document.getElementById('card-catalog')
 
 function Book(title, author, pages, read) {
     this.title = title
@@ -12,6 +11,11 @@ function Book(title, author, pages, read) {
     this.pages = pages
     this.read = read
 }
+
+// test books
+library.push(new Book('The DaVinci Code', 'Dan Brown', 384, true))
+library.push(new Book('The Hobbit', 'J.R.R. Tolkien', 295, false))
+library.push(new Book('Mitt Liv', 'Lars Monsen', 246, false))
 
 function addBookToLibrary() {
     library.push(new Book(form.elements[0].value,               //title
@@ -52,9 +56,27 @@ function hideForm() {
     formOverlay.style.display = 'none'
 }
 
-library.push(new Book('The DaVinci Code', 'Dan Brown', 384, true))
-library.push(new Book('The Hobbit', 'J.R.R. Tolkien', 295, false))
-library.push(new Book('Mitt Liv', 'Lars Monsen', 246, false))
+function createStoredCards() {
+    for (let book of library) {
+        createCard(book)
+    }
+}
+
+function createCard (book) {
+    let newCard = document.createElement('div')
+    newCard.innerHTML = `<b>${book.title}</b><br>
+                          by ${book.author}<br>
+                          ${book.pages} pg<br>
+                          ${book.read ? 'read': 'not read'}`
+    newCard.classList.add('library-card')
+    cardCatalog.appendChild(newCard)
+
+}
+
+createStoredCards()
+// TODO: mark as read
+
+// TODO: delete book
 
 btnAddBook.addEventListener('click', () => {
     if (form.elements[0].value && form.elements[1].value && (parseInt(form.elements[2].value) > 0)) {
